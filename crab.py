8111929d6be3bbf58bc494bedeb10442db607e48
jamesw98 <xeekatar@gmail.com>
2020-11-29 19:00:34 -0500

    testing mention

--- crab.py
+++ crab.py
@@ -24,12 +24,17 @@ async def on_message(message):
     # whether or not it is time for crab or not
     tfc = random.randint(1,1000) % 3 == 0
     
-    # if the message has
+    # if the message has "crab" in it, there is a chance the bot 
+    # will react with a crab emoji
     if "crab" in (message.content.lower()) and tfc:
         await message.add_reaction("🦀")
 
+    # parses a crab command
     if (message.content[0:6] == "!crab "):
         cmd = message.content[6:]
         await message.channel.send(parse_cmd(cmd))
+    
+    if ("<@!598693241134645267>" in message.content):
+        await message.channel.send("🦀How dare you ping your crab overlord!🦀")
 
 client.run(TOKEN)
